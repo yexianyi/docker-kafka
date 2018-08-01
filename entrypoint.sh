91d3a478596f2c8a1ef503394355804575e4d3b2
Xianyi Ye <yexianyi@hotmail.com>
2018-08-01 12:35:25 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,3 @@
-bin/zookeeper-server-start.sh config/zookeeper.properties &
-bin/kafka-server-start.sh config/server.properties &
+#!/bin/bash
+bin/zookeeper-server-start.sh config/zookeeper.properties >> zookeeper.log &
+bin/kafka-server-start.sh config/server.properties >> kafka.log
