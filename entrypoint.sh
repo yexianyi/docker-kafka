e3bc80b9265ac78426041bf3fca9fb646809f379
Xianyi Ye <yexianyi@hotmail.com>
2018-08-01 13:22:12 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+sed -i "s/#advertised.listeners=PLAINTEXT:\/\/your.host.name:9092/advertised.listeners=PLAINTEXT:\/\/localhost:9092/" config/server.properties
 bin/zookeeper-server-start.sh config/zookeeper.properties >> zookeeper.log &
 bin/kafka-server-start.sh config/server.properties >> kafka.log
