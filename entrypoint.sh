1ce467daf019c9fef559d770db7216a80e962577
Xianyi Ye <yexianyi@hotmail.com>
2018-08-02 11:24:47 +0800

    enhance capability to launch kafka cluster in single node

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
-sed -i "s/#advertised.listeners=PLAINTEXT:\/\/your.host.name:9092/advertised.listeners=PLAINTEXT:\/\/localhost:9092/" config/server.properties
-bin/zookeeper-server-start.sh config/zookeeper.properties >> zookeeper.log &
-bin/kafka-server-start.sh config/server.properties >> kafka.log
+partitions=${PARTITION:-1}
+replica=${REPLICA:-1}
+brokers=${BROKERS:-1}
+
+#Launch Zookeeper
+nohup bin/zookeeper-server-start.sh config/zookeeper.properties > zookeeper.log &
+
+for((i=0;i<brokers;i++));
+    do
+        #config kafka for each broker
+        cp config/server.properties config/server-$i.properties
+        sed -i "s/broker.id=0/broker.id=${i}/" config/server-$i.properties
+        sed -i "s/#listeners=PLAINTEXT:\/\/:9092/listeners=PLAINTEXT:\/\/:909$((2+i))/" config/server-$i.properties
+        sed -i "s/#advertised.listeners=PLAINTEXT:\/\/your.host.name:9092/advertised.listeners=PLAINTEXT:\/\/localhost:909$((2+i))/" config/server-$i.properties
+        sed -i "s/log.dirs=\/tmp\/kafka-logs/log.dirs=\/tmp\/kafka-logs-${i}/" config/server-$i.properties
+
+        #Launch server {i}
+        nohup bin/kafka-server-start.sh config/server-$i.properties > kafka-$i.log &
+    done
+
+exit 0
